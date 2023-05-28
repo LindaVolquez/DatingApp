121995650c85783265d3b4d446a9af040f997455
LindaVolquez <lindae.volquez@gmail.com>
2023-05-28 13:13:51 -0600

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.MapControllers(); // this is the middlerware - This one is used to map the endpoints so when a request is received this provide the endpoint
 
 app.Run(); // This is the command that actually run the application - this unit Program.cs is the entry point to the application
